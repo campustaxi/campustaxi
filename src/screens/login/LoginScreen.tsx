a5c99abc1e7b802b5812fcbc634c8bf33aa14c37
kang-heewon <heewon.dev@gmail.com>
2021-01-25 01:32:32 +0900

    feat: 애플로그인 구현 on ios

--- src/screens/login/LoginScreen.tsx
+++ src/screens/login/LoginScreen.tsx
@@ -1,8 +1,9 @@
 import styled from '@emotion/native';
+import appleAuth from '@invertase/react-native-apple-authentication';
 import KakaoLogins, { KAKAO_AUTH_TYPES } from '@react-native-seoul/kakao-login';
 import { StackNavigationProp } from '@react-navigation/stack';
 import React, { useState } from 'react';
-import { SafeAreaView } from 'react-native';
+import { Platform, SafeAreaView } from 'react-native';
 import image from '../../../images/login/bg.png';
 import { BaseButton } from '../../components/button/BaseButton';
 import { KakaoIcon } from '../../components/icon/KakaoIcon';
@@ -18,7 +19,24 @@ type Props = {
 export const LoginScreen: React.FC<Props> = () => {
   const [isSimpleLogin, setIsSimpleLogin] = useState(false);
   const { setLoggedIn } = useAuthContext();
+  const handleAppleLogin = async () => {
+    // performs login request
+    const appleAuthRequestResponse = await appleAuth.performRequest({
+      requestedOperation: appleAuth.Operation.LOGIN,
+      requestedScopes: [appleAuth.Scope.EMAIL, appleAuth.Scope.FULL_NAME],
+    });
 
+    // get current authentication state for user
+    // /!\ This method must be tested on a real device. On the iOS simulator it always throws an error.
+    const credentialState = await appleAuth.getCredentialStateForUser(
+      appleAuthRequestResponse.user,
+    );
+
+    // use credentialState response to ensure the user is authenticated
+    if (credentialState === appleAuth.State.AUTHORIZED) {
+      // user is authenticated
+    }
+  };
   return isSimpleLogin ? (
     <Background source={image}>
       <SafeAreaView>
@@ -42,6 +60,11 @@ export const LoginScreen: React.FC<Props> = () => {
               icon={<KakaoIcon />}>
               카카오 로그인
             </BaseButton>
+            {Platform.OS === 'ios' && (
+              <BaseButton onPress={handleAppleLogin} backgroundColor="white" icon={<KakaoIcon />}>
+                애플 로그인
+              </BaseButton>
+            )}
             <BaseButton onPress={() => setIsSimpleLogin(true)}>일반 로그인 및 회원가입</BaseButton>
           </LoginButtonContainer>
         </LoginContainer>
