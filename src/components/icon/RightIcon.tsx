d936f3317b15235b566decfc5152de279a8db18c
kang-heewon <heewon.dev@gmail.com>
2021-02-23 11:39:18 +0900

    feat: 설정 화면

---
+++ src/components/icon/RightIcon.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Svg, { G, Path } from 'react-native-svg';
+
+export const RightIcon: React.FC = ({}) => {
+  return (
+    <Svg width="9.15" height="15.471" viewBox="0 0 9.15 15.471">
+      <G transform="translate(7.736 14.764) rotate(180)">
+        <Path d="M7.029,0,0,7.029l7.029,7.029" fill="none" stroke="#000" stroke-width="2" />
+      </G>
+    </Svg>
+  );
+};
